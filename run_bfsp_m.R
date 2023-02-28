23cf3bef1a100e974880387c82954ea03d6ac3fe
Maria Masotti <112586949+mariamasotti1@users.noreply.github.com>
2023-02-28 13:58:55 -0500

    Update run_bfsp_m.R

--- run_bfsp_m.R
+++ run_bfsp_m.R
@@ -3,4 +3,4 @@ data<-read.csv("example_data.csv") #read in data
 set.seed(1) #set random seed for reproducible results 
 result<-bfspm(data$x,data$y,data$mcoord,iterations=10000,burn=2000,max_lesion=4,spatial=F) #run bfsp-m algorithm
 mask<-post_process_bfspm(result,prob = .5) #post-process 
-write.csv(result,"bfsp_m_results.csv") #save results as .csv file 
+write.csv(mask,"bfsp_m_results.csv") #save results as .csv file 
