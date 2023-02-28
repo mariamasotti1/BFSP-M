df11d9e4fab9fadf7fa4d1c860bc0cb95ed393be
Maria Masotti <112586949+mariamasotti1@users.noreply.github.com>
2023-02-28 13:57:33 -0500

    Update run_bfsp_m.R

--- run_bfsp_m.R
+++ run_bfsp_m.R
@@ -1,3 +1,4 @@
+source("bfsp_m.csv")
 data<-read.csv("example_data.csv") #read in data
 set.seed(1) #set random seed for reproducible results 
 result<-bfspm(data$x,data$y,data$mcoord,iterations=10000,burn=2000,max_lesion=4,spatial=F) #run bfsp-m algorithm
